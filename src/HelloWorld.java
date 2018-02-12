0ce25e25876e5b183ef7ff8944e5e0452230bb89
sradder61 <sradder@aus.edu>
2018-02-12 14:57:10 +0400

    fautre B added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 
 public class HelloWorld {
-
+   
+	public void addFeatureB()
+	{
+		System.out.println("Faeture A added");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Hello World");
